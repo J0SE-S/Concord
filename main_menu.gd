8ba059641aa728b9728e9f51a98fcd9f13cbb8f4
J0SE-S <joseph.simutis@icloud.com>
2023-09-14 20:16:22 -0400

    Adding Main Menu
    TODO: Add functionality to the Play and Settings buttons

---
+++ main_menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var gameScene : PackedScene
+
+func _on_play_button_button_up():
+	pass # Replace with function body.
+
+
+func _on_settings_button_button_up():
+	pass # Replace with function body.
+
+
+func _on_quit_button_button_up():
+	get_tree().quit()
