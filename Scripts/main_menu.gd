30f3fcce4c17dda3f36ebb1b40ce13f9e46a11fe
J0SE-S <joseph.simutis@icloud.com>
2023-09-15 17:50:04 -0400

    reshuffled the directory system i think
    i dont remember

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var gameScene : PackedScene
 
 func _on_play_button_button_up():
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(gameScene)
 
 
 func _on_settings_button_button_up():
