a5cde8e45876bcaff324b974cccff3c012440d4f
J0SE-S <joseph.simutis@icloud.com>
2023-09-17 23:19:51 -0400

    Added splash screen, some bugfixes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,17 +1,24 @@
 extends CharacterBody2D
 
-@export var cam: Camera2D
+@export var cam : Camera2D
+@export var name_tag : Label
 
 const SPEED = 100.0
 
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+
 func _ready():
-	set_multiplayer_authority(name.to_int())
-	cam.set_enabled(is_multiplayer_authority())
+	if (is_multiplayer_authority()):
+		cam.make_current()
+		
+#func set_name_tag(new_name):
+	#name_tag.text = str(new_name)
 
 func _process(_delta):
 	if is_multiplayer_authority():
 		if Input.is_action_pressed("exit"):
-			$"../".exit_game(name)
+			get_tree().current_scene.exit_game(name)
 			pass
 		
 		var direction : Vector2 = Vector2(
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 var peer = ENetMultiplayerPeer.new()
-@export var player_scene: PackedScene
-@export var network_objects_node: Node2D
-@onready var cam = $Camera2D
+@export var player_scene : PackedScene
+@export var network_objects_node : Node2D
+@export var cam : Camera2D
+@export var spawn_positions : PackedVector2Array
+var user_key : String
+var server_key : String
+
+func _ready():
+	var auth = FileAccess.open("user://auth.dat", FileAccess.READ)
+	user_key = auth.get_line()
+	server_key = auth.get_line()
+	auth.close()
 
 func _on_play_button_button_up():
 	#if (is_server):
@@ -11,14 +20,14 @@ func _on_play_button_button_up():
 	multiplayer.peer_connected.connect(add_player)
 	add_player()
 	multiplayer.multiplayer_peer = peer
-	cam.enabled = false
+	#cam.enabled = false
 	#else:
 	#	peer.create_client("127.0.0.1", 10273)
 	#	multiplayer.multiplayer_peer = peer
 	#	cam.enabled = false
 
 func _on_settings_button_button_up():
-	peer.create_client("127.0.0.1", 10273)
+	peer.create_client(server_key.hex_decode().get_string_from_utf8(), 10273)
 	multiplayer.multiplayer_peer = peer
 	cam.enabled = false
 
@@ -28,15 +37,13 @@ func _on_quit_button_button_up():
 func add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
+	#player.set_name_tag(user_key)
 	network_objects_node.add_child(player)
 
 func exit_game(id):
-	multiplayer.peer_disconnected.connect(del_player)
-	del_player(id)
+	rpc("del_player", id)
 	get_tree().quit()
 
+@rpc("any_peer", "call_local", "reliable")
 func del_player(id):
-	rpc("_del_player", id)
-	
-@rpc("any_peer", "call_local") func _del_player(id):
-	get_node(str(id)).queue_free()
+	network_objects_node.get_node(str(id)).queue_free()
